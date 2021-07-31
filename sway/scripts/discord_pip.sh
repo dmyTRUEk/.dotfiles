258fa97523063d5860451cf783c2855f60ad7a9b
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2021-07-31 17:37:53 +0300

    made discord picture-in-picture automatic

---
+++ sway/scripts/discord_pip.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# This script makes discord picture-in-picture window
+
+# if its broken, use mkdir ~/test/updater_lt6_ge6 to see what works, and what no
+if [ $(swaymsg -t get_tree | grep 'discord updater' -i | wc -l) -gt 0 ]; then
+    true;
+elif [ $(swaymsg -t get_tree | grep 'discord' -i | wc -l) -lt 6 ]; then
+    swaymsg "[class=\"discord\"]" floating disable, sticky disable;
+elif [ $(swaymsg -t get_tree | grep 'discord' -i | wc -l) -ge 6 ]; then
+    sleep 0.4;
+    swaymsg "[class=\"discord\"]" floating enable, sticky enable;
+    swaymsg "[class=\"discord\" title=\"Discord\"]" floating disable, sticky disable;
+fi #> ~/test/ok.txt 2> ~/test/error.txt
+
+
+

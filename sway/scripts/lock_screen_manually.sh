ccc147842f77f1c12a499e17b9e50b2924db03ff
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2021-09-04 10:26:21 +0300

    fixed lockscreen wallpaper name

--- sway/scripts/lock_screen_manually.sh
+++ sway/scripts/lock_screen_manually.sh
@@ -4,7 +4,7 @@
 
 
 
-lockscreen_wallpaper=$HOME/Dropbox/Pictures/Wallpapers/art/solis-2560×1440.jpg
+lockscreen_wallpaper=$HOME/Dropbox/Pictures/Wallpapers/art/solis-2560x1440.jpg
 delay_turn_off_screen=10
 tmp_file=/tmp/for_sway_scripts__lock_screen_manually__swayidle_pid.txt
 

f0efc013a53552a7c0fc99791f4ce5af77777aab
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2021-09-14 22:56:05 +0300

    added `nvimura` script, `$(xdg-user-dir)` -> `$HOME`

--- setup-aliases.sh
+++ setup-aliases.sh
@@ -10,20 +10,22 @@
 # TODO: rewrite using functions
 
 # My Custom ALIASes:
-alias_name="ff" && alias_path=$(xdg-user-dir)/.local/bin/$alias_name && echo -e "#\!/bin/sh\nexec firefox" > $alias_path && chmod +x $alias_path
+alias_name="ff" && alias_path=$HOME/.local/bin/$alias_name && echo -e "#\!/bin/sh\nexec firefox" > $alias_path && chmod +x $alias_path
 
-alias_name="tg" && alias_path=$(xdg-user-dir)/.local/bin/$alias_name && echo -e "#\!/bin/sh\nXCURSOR_THEME=breeze_cursors\nexec telegram-desktop" > $alias_path && chmod +x $alias_path
+alias_name="tg" && alias_path=$HOME/.local/bin/$alias_name && echo -e "#\!/bin/sh\nXCURSOR_THEME=breeze_cursors\nexec telegram-desktop" > $alias_path && chmod +x $alias_path
 
-alias_name="fm" && alias_path=$(xdg-user-dir)/.local/bin/$alias_name && echo -e "#\!/bin/sh\nXCURSOR_THEME=breeze_cursors\nexec dolphin" > $alias_path && chmod +x $alias_path
+alias_name="fm" && alias_path=$HOME/.local/bin/$alias_name && echo -e "#\!/bin/sh\nXCURSOR_THEME=breeze_cursors\nexec dolphin" > $alias_path && chmod +x $alias_path
 
-alias_name="pm" && alias_path=$(xdg-user-dir)/.local/bin/$alias_name && echo -e "#\!/bin/sh\nexec pamac-manager" > $alias_path && chmod +x $alias_path
+alias_name="pm" && alias_path=$HOME/.local/bin/$alias_name && echo -e "#\!/bin/sh\nexec pamac-manager" > $alias_path && chmod +x $alias_path
 
-alias_name="sm" && alias_path=$(xdg-user-dir)/.local/bin/$alias_name && echo -e "#\!/bin/sh\nexec plasma-systemmonitor" > $alias_path && chmod +x $alias_path
+alias_name="sm" && alias_path=$HOME/.local/bin/$alias_name && echo -e "#\!/bin/sh\nexec plasma-systemmonitor" > $alias_path && chmod +x $alias_path
 
-alias_name="ds" && alias_path=$(xdg-user-dir)/.local/bin/$alias_name && echo -e "#\!/bin/sh\nexec discord" > $alias_path && chmod +x $alias_path
+alias_name="ds" && alias_path=$HOME/.local/bin/$alias_name && echo -e "#\!/bin/sh\nexec discord" > $alias_path && chmod +x $alias_path
 
-alias_name="anst" && alias_path=$(xdg-user-dir)/.local/bin/$alias_name && echo -e "#\!/bin/sh\n_JAVA_AWT_WM_NONREPARENTING=1 android-studio" > $alias_path && chmod +x $alias_path
+alias_name="anst" && alias_path=$HOME/.local/bin/$alias_name && echo -e "#\!/bin/sh\n_JAVA_AWT_WM_NONREPARENTING=1 android-studio" > $alias_path && chmod +x $alias_path
 
-alias_name="pych" && alias_path=$(xdg-user-dir)/.local/bin/$alias_name && echo -e "#\!/bin/sh\n_JAVA_AWT_WM_NONREPARENTING=1 pycharm" > $alias_path && chmod +x $alias_path
+alias_name="pych" && alias_path=$HOME/.local/bin/$alias_name && echo -e "#\!/bin/sh\n_JAVA_AWT_WM_NONREPARENTING=1 pycharm" > $alias_path && chmod +x $alias_path
 
+# TODO: set column number
+alias_name="nvimura" && alias_path=$HOME/.local/bin/$alias_name && echo -e "#!/bin/sh\nzathura -x \"nvr --remote \\\"+call cursor(%{line}, 0)\\\" %{input}\" \$1" > $alias_path && chmod +x $alias_path
 

9424dcd53d45c11732fbc08ec153b0377fc45a10
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2022-02-09 20:50:05 +0200

    update `ranger` colorscheme

--- ranger/colorschemes/gruvbox.py
+++ ranger/colorschemes/gruvbox.py
@@ -3,22 +3,25 @@
 from __future__ import (absolute_import, division, print_function)
 
 from ranger.colorschemes.default import Default
-from ranger.gui.color import red, blue, yellow
-
+from ranger.gui.color import red, blue, yellow, black
 
 
 class Scheme(Default):
-    progress_bar_color = yellow
+    accent_color = yellow
+    progress_bar_color = accent_color
 
     def use(self, context):
         fg, bg, attr = Default.use(self, context)
 
-        if context.directory and not context.marked and not context.link \
-                and not context.inactive_pane:
-            fg = self.progress_bar_color
+        if context.directory and not context.marked and not context.link and not context.inactive_pane:
+            fg = self.accent_color
 
         if context.in_titlebar and context.hostname:
             fg = red if context.bad else blue
+
+        if context.tab and context.good:
+            bg = self.accent_color
+            fg = black
 
         return fg, bg, attr
 

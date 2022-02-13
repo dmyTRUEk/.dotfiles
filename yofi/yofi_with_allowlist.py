69876a015fdcd4df39eb4f7af56bd060029fdae5
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2022-02-13 23:49:51 +0200

    added `yofi` with allowlist script

---
+++ yofi/yofi_with_allowlist.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import subprocess
+import os
+
+apps = {
+    'Alacritty': 'alacritty',
+    'Android Studio': '_JAVA_AWT_WM_NONREPARENTING=1 android-studio',
+    'Discord': 'discord',
+    'Firefox': 'firefox',
+    'Image Viewer': 'imv',
+    'Inkscape': 'inkscape',
+    'Krita': 'krita',
+    'Libre Office': 'libreoffice',
+    'OBS Studio': 'obs',
+    'SciDAVis': 'scidavis',
+    'Telegram Desktop': 'telegram-desktop',
+    'Zathura': 'zathura',
+}
+
+app_names_list = list(apps)
+app_names_str = '\n'.join(app_names_list)
+
+command_yofi = f'yofi dialog <<< "{app_names_str}"'
+app_name_to_launch = subprocess.getoutput(command_yofi)
+app_bin_to_launch = apps[app_name_to_launch]
+
+# Pass the final command to swaymsg so that the resulting window can be opened on the original workspace that the command was run on.
+command_exec = f'echo {app_bin_to_launch} | xargs swaymsg exec --'
+os.system(command_exec)
+

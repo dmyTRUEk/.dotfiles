5a6ba43ec8cb45d056533c195dcdde49897cc06b
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2022-03-27 22:30:45 +0300

    `ideavim`: create

--- setup-file-links.sh
+++ setup-file-links.sh
@@ -11,6 +11,7 @@
 
 # files:
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/.ideavimrc ~/.ideavimrc
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.zprofile ~/.zprofile
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc

81534d1d6c70a0b2970e642b5828eedf7ddbc64b
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2022-05-22 10:06:22 +0300

    `yofi`: change font, refactor

--- yofi/yofi_with_allowlist.py
+++ yofi/yofi_with_allowlist.py
@@ -4,29 +4,30 @@ import subprocess
 import os
 
 apps = {
-    'Alacritty': 'alacritty',
-    'Android Studio': '_JAVA_AWT_WM_NONREPARENTING=1 android-studio',
-    'Discord': 'discord',
-    'Discord Canary': 'discord-canary',
-    'Firefox': 'firefox',
-    'Image Viewer': 'imv',
-    'Inkscape': 'inkscape',
-    'Krita': 'krita',
-    'Libre Office': 'libreoffice',
-    'OBS Studio': 'obs',
-    'SciDAVis': 'scidavis',
-    'Telegram Desktop': 'telegram-desktop',
-    'Zathura': 'zathura',
+    "Alacritty": "alacritty",
+    "Android Studio": "_JAVA_AWT_WM_NONREPARENTING=1 android-studio",
+    "Discord": "discord",
+    "Discord Canary": "discord-canary",
+    "Firefox": "firefox",
+    "Image Viewer": "imv",
+    "Inkscape": "inkscape",
+    "Krita": "krita",
+    "Libre Office": "libreoffice",
+    "OBS Studio": "obs",
+    "SciDAVis": "scidavis",
+    "Telegram Desktop": "telegram-desktop",
+    "Steam": "steam",
+    "Zathura": "zathura",
 }
 
 app_names_list = list(apps)
 app_names_str = '\n'.join(app_names_list)
 
-command_yofi = f'yofi dialog <<< "{app_names_str}"'
+command_yofi = f'/home/myshko/.cargo/bin/yofi dialog <<< "{app_names_str}"'
 app_name_to_launch = subprocess.getoutput(command_yofi)
 app_bin_to_launch = apps[app_name_to_launch]
 
 # Pass the final command to swaymsg so that the resulting window can be opened on the original workspace that the command was run on.
-command_exec = f'echo {app_bin_to_launch} | xargs swaymsg exec --'
+command_exec = f"echo {app_bin_to_launch} | xargs swaymsg exec --"
 os.system(command_exec)
 

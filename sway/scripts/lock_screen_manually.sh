c16ff1a242def112e4ecf08c5e1ce4e10647157e
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2022-05-22 10:25:52 +0300

    `swaylock`: update lockscreen config and setup

--- sway/scripts/lock_screen_manually.sh
+++ sway/scripts/lock_screen_manually.sh
@@ -4,20 +4,14 @@
 
 delay_turn_off_screen=10
 
-tmp_file=/tmp/for_sway_scripts__lock_screen_manually__swayidle_pid.txt
+swayidle -w \
+    timeout $delay_turn_off_screen 'swaymsg "output * dpms off"' \
+    resume 'swaymsg "output * dpms on"' &
 
-{
-    swaylock
-    # while `swaylock` is on the command is not finished,
-    # so kill will be executed only when the screen is unlocked
-    this_swayidle_pid="$(cat $tmp_file)"
-    kill -9 $this_swayidle_pid
-} & \
-{
-    swayidle -w \
-        timeout $delay_turn_off_screen 'swaymsg "output * dpms off"' \
-        resume 'swaymsg "output * dpms on"' \
-    & \
-    this_swayidle_pid=$! && echo $this_swayidle_pid > $tmp_file
-}
+swaylock
+# While `swaylock` is on, the command is not finished,
+# so kill will be executed only when the screen is unlocked.
+pkill swayidle
+
+exec swayidle -w before-sleep '/home/myshko/.config/sway/scripts/lock_screen_manually.sh'
 

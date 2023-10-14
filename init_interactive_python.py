ea1ca6d15a6eace0d09f43aad3c8131473bd9fd4
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2023-10-14 16:51:13 +0300

    `ipy`: refactor pipe imports, add pipes for `avg` & `enumerate`

--- init_interactive_python.py
+++ init_interactive_python.py
@@ -11,7 +11,31 @@ import itertools
 
 import numpy as np
 
-from pipe import Pipe, take as take_, tail as tail_, skip as skip_, dedup as dedup_, uniq as uniq_, permutations as permutations_, traverse as traverse_, tee as tee_, map as map_, filter as filter_leave_, take_while as take_while_, skip_while as skip_while_, groupby as group_by_, sort as sort_, reverse as reverse_, t as append_, transpose as transpose_, chain as chain_, chain_with as chain_with_, islice as slice_, izip as zip_
+from pipe import (
+    Pipe,
+    chain as chain_,
+    chain_with as chain_with_,
+    dedup as dedup_,
+    # enumerate as enumerate_, # on main, but not in released version
+    filter as filter_leave_,
+    groupby as group_by_,
+    islice as slice_,
+    izip as zip_,
+    map as map_,
+    permutations as permutations_,
+    reverse as reverse_,
+    skip as skip_,
+    skip_while as skip_while_,
+    sort as sort_,
+    t as append_,
+    tail as tail_,
+    take as take_,
+    take_while as take_while_,
+    tee as tee_,
+    transpose as transpose_,
+    traverse as traverse_,
+    uniq as uniq_,
+)
 
 
 # abcdefghijklmnopqrstuvwxyz
@@ -43,7 +67,7 @@ def split_at(l: list["T"], index: int) -> tuple[list["T"], list["T"]]: # pyright
 def split_at_percentage(l: list["T"], p: float) -> tuple[list["T"], list["T"]]: # pyright: ignore[reportUndefinedVariable]
     return split_at(l, round(len(l) * p))
 
-def std_dev(l: list) -> float:
+def std_dev(l: list[float]) -> float:
     avg_value = avg(l)
     return sqrt( sum(map(lambda x: (x-avg_value)**2, l)) / (len(l) - 1) )
 
@@ -53,6 +77,8 @@ def unzip2(xy: Iterator) -> tuple[Iterator, Iterator]:
 
 
 # Pipes:
+avg_ = Pipe(avg)
+enumerate_ = Pipe(enumerate)
 filter_remove_ = Pipe(filter_remove)
 index_of_max_ = Pipe(index_of_max)
 list_ = Pipe(list)
